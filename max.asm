636d47f90e54fc8de062bb0c881d40a80f57ac02
mahaboobtech <52873899+mahaboobtech@users.noreply.github.com>
2021-11-14 13:31:54 +0530

    Add files via upload

---
+++ max.asm
@@ -0,0 +1,23 @@
+assume ds:data , cs:code
+
+data segment
+n1 dw 1234h, 456h, 0abcdh, 2678h
+max dw ?
+data ends
+
+code segment
+start: mov ax, data
+       mov ds, ax
+       lea si, [n1]
+       mov ax, [si]
+       mov cx, 03h
+    x: inc si
+       inc si
+       cmp ax, [si]
+       jnc y
+       mov ax, [si]
+    y: loop x
+       mov max, ax 
+       int 03h
+       code ends
+       end start
